6e60c71ed83a7e4a0715a0ae0d6e63f6a8799759
dittophantassy@gmail.com <dittophantassy@gmail.com>
2026-09-01 18:41:59 -0300

    añadi declaraciones de interés

---
+++ web/src/data/declaracionesDeInteres.js
@@ -0,0 +1,253 @@
+const DECLARACIONES_DE_INTERES = [
+    {
+        categoria: 'Provincia',
+        declaraciones: [
+            {
+                titulo: 'Legislatura de la Provincia de Córdoba',
+                fecha: '2026-04-08',
+                link: 'https://drive.google.com/file/d/1tziF-VpefWnCVeBZLf7fqjl-XSAOlaeT',
+            },
+            {
+                titulo: 'Secretaría de La Mujer',
+                fecha: '2026-04-22',
+                link: 'https://drive.google.com/file/d/1e4qtfrknPVLZx9kxr16jXQol7vzHqNGZ',
+            },
+            {
+                titulo: 'Comisión Provincial de la Memoria Córdoba',
+                fecha: '2026-04-28',
+                link: 'https://drive.google.com/file/d/1YQ3Q9E88C8D_X0GqdeYvjpojkMnkGxZ0',
+            },
+        ]
+    },
+    {
+        categoria: 'Comunas y Municipios',
+        declaraciones: [
+            {
+                titulo: 'Municipalidad de Aguas de Oro',
+                fecha: '2026-03-12',
+                link: 'https://drive.google.com/file/d/1UCXMQP9cbeq84qKvbikBnaCueCD8haNO',
+            },
+            {
+                titulo: 'Comuna Cabalango',
+                fecha: '2026-05-21',
+                link: 'https://drive.google.com/file/d/1hfhRUgS3l54Lbh8iH9NLeg_Tggb9FKQe',
+            },
+            {
+                titulo: 'Municipalidad de Pilar',
+                fecha: '2026-04-21',
+                link: 'https://drive.google.com/file/d/1TOPXQzdEDDQIuzsuM0HgOtGGIXpKcE-e',
+            },
+            {
+                titulo: 'Concejo Deliberante de la Ciudad de Córdoba',
+                fecha: '2026-05-21',
+                link: 'https://drive.google.com/file/d/1NbtYPUgLU_YbWf9l3QHKv4aIMjayj5PF',
+            },
+            {
+                titulo: 'Concejo Deliberante de la Ciudad de Río Cuarto',
+                fecha: '2026-04-07',
+                link: 'https://drive.google.com/file/d/1NlA2gD-bDE4W9byjKYAqHcosEbeSyAjC',
+            },
+            {
+                titulo: 'Municipalidad de Córdoba',
+                fecha: '2026-04-10',
+                link: 'https://drive.google.com/file/d/1BZyWHt7VOsNUgWahxm7x6S9XXfjI1wRg',
+            },
+            {
+                titulo: 'Concejo Deliberante Municipalidad de Tanti',
+                fecha: '2026-06-24',
+                link: 'https://drive.google.com/file/d/11riPeA92X-pDIWU6Jx3cT8QiM-RX-HJO',
+            },
+            {
+                titulo: 'Municipalidad de Santa María de Punilla',
+                fecha: '2026-07-01',
+                link: 'https://drive.google.com/file/d/1r7_sFZoTAfw9nx0vSDZnPAT-A3FObp_O',
+            },
+            {
+                titulo: 'Concejo Deliberante de la Ciudad de Río Ceballos',
+                fecha: '2026-07-03',
+                link: 'https://drive.google.com/file/d/1ORBAC84DRhIol-gEKCzHPPwSxs860stf',
+            },
+            {
+                titulo: 'Concejo Deliberante de Municipalidad de Unquillo',
+                fecha: '2026-05-12',
+                link: 'https://drive.google.com/file/d/1K8jTd_qxgAZtHGxgi_uHQH7EWyQQ4tef',
+            },
+            {
+                titulo: 'Comisión Comunal De Villa Ciudad Parque',
+                fecha: '2026-06-22',
+                link: 'https://drive.google.com/file/d/1dN7oV-kfY9lduD9VWQnCTzAxGyDSzeku',
+            },
+            {
+                titulo: 'Concejo Deliberante de Villa María',
+                fecha: '2026-05-11',
+                link: 'https://drive.google.com/file/d/1cmpFmZxPhWDILq3xtuu-idZX7_s6ioqF',
+            },
+            {
+                titulo: 'Concejo Deliberante de la Municipalodad de Salsipuedes',
+                fecha: '2026-0507',
+                link: 'https://drive.google.com/file/d/1rJBlHXad6Ux-c61WL4dN0UeKoDXtGlj2',
+            },
+            {
+                titulo: 'Comisión Comunal de Los Reartes',
+                fecha: '2026-07-06',
+                link: 'https://drive.google.com/file/d/1agK6yIMsxpzqr8_zWZwXZyc9ee8c2y7m',
+            },
+            {
+                titulo: 'Honorable Concejo Deliberante Villa Yacanto de Calamuchita',
+                fecha: '2026-08-06',
+                link: 'https://drive.google.com/file/d/1V6srs-ZGc-gXLZxCxiMKIfXEL7tAKEsM',
+            },
+            {
+                titulo: 'Concejo Deliberante de Santa Rosa de Calamuchita',
+                fecha: '2026-08-13',
+                link: 'https://drive.google.com/file/d/1N-6MOZHZd_TR19w7vexxXcWeUFcgUobd',
+            },
+            {
+                titulo: 'Concejo Deliberante de la Ciudad de Saldán',
+                fecha: '2026-07-09',
+                link: 'https://drive.google.com/file/d/1HZFEDA3CvE0rY1m6QRafHG31rGoxUzVC',
+            }
+        ]
+    },
+    {
+        categoria: 'Universidades',
+        declaraciones: [
+            {
+                titulo: 'Facultad de Filosofía y Humanidades Universidad Nacional de Córdoba',
+                fecha: '2026-04-24',
+                link: 'https://drive.google.com/file/d/1Er2dcOvKXErUfLw9WpEYpB5IAF2IcFMo',
+            },
+            {
+                titulo: 'Universidad Provincial de Córdoba',
+                fecha: '2026-04-06',
+                link: 'https://drive.google.com/file/d/1EXcN8C8aCMl17S_q0EddHyO1TugnpU5y',
+            },
+            {
+                titulo: 'Universidad Nacional de Villa María',
+                fecha: '2026-05-06',
+                link: 'https://drive.google.com/file/d/1nX9nRxsLUyHu3CA8a8S1cCT604WflCg_',
+            },
+            {
+                titulo: 'Facultad de Ciencias de la Comunicación Universidad Nacional de Córdoba',
+                fecha: '2026-03-11',
+                link: 'https://drive.google.com/file/d/1aVL2JQkBYvWa7PlfLWMuH9IC5nqEH2fA',
+            },
+            {
+                titulo: 'Facultad de Derecho Universidad Nacional de Córdoba',
+                fecha: '2026-07-02',
+                link: 'https://drive.google.com/file/d/1RPoBkGRmalf7q-LMvogs8uL1Jxdim8_L',
+            },
+        ]
+    },
+    {
+        categoria: 'Sindicatos',
+        declaraciones: [
+            {
+                titulo: 'Sindicato LGBT de trabajadoras y trabajadores Luis Gauthier',
+                fecha: '2026-05-11',
+                link: 'https://drive.google.com/file/d/1lNexxDPG8BRPgT-mi170lSLs0W3bKWm_',
+            },
+            {
+                titulo: 'Coordinadora Nacional de Trabajadoras en Domicilio de Chile, CONATRADO',
+                fecha: '2026-05-13',
+                link: 'https://drive.google.com/file/d/1-FnQfVEptXymqbzShDTF64e_-SXijwsP',
+            },
+            {
+                titulo: 'Sindicato Unión Obreros y Empleados Municipales, SUOEM',
+                fecha: '2026-04-30',
+                link: 'https://drive.google.com/file/d/1IIzCrTlBGMzSvROpB4k8u0sXnrS_t0Z7',
+            },
+            {
+                titulo: 'Asociación de Trabajadores del Estado, ATE',
+                fecha: '2026-03-25',
+                link: 'https://drive.google.com/file/d/1G8koGhTr-YuZrlGBZyhHq2KAmqE8QiUb',
+            },
+            {
+                titulo: 'Central de Trabajadores de la Argentina Autónoma, ATE CTA Autónoma',
+                fecha: '2026-03-25',
+                link: 'https://drive.google.com/file/d/1vcJUuQd7CQqV9kiEsgNVq3J1MyAc4GvV',
+            },
+            {
+                titulo: 'Confederación de Trabajadores Municipales de la República Argentina, CTM',
+                fecha: '2026-07-31',
+                link: 'https://drive.google.com/file/d/14Lgch25orIr5N5sVQRj0kb2G9SjKsjhK',
+            },
+            {
+                titulo: 'Sindicato Argentino de Docentes Privados, SADOP',
+                fecha: '2026-08-27',
+                link: 'https://drive.google.com/file/d/1s6tJm9UyFN_f8tfBW44Q1OErS7KKVNnR',
+            },
+            {
+                titulo: 'Asociación Gremial de Empleados del Poder Judicial de Córdoba, AGEPJ',
+                fecha: '2026-08-26',
+                link: 'https://drive.google.com/file/d/1jqtzyoXaHD-ukMkq7qm4aOMtfJKRbc8g',
+            },
+            {
+                titulo: 'Federación de Sindicatos de Municipales Unidos de la Provincia de Córdoba, FESIMUPC',
+                fecha: '2026-08-31',
+                link: 'https://drive.google.com/file/d/1po5xYou3GvvcNhHAsiK3QY_ifAvdqB9B',
+            },
+            {
+                titulo: 'Circulo Sindical de la Prensa y la Comunicación de Córdoba, CISPREN',
+                fecha: '2026-08-31',
+                link: 'https://docs.google.com/document/d/1CfJbAPHFDHWKeK-76oEpIuC9aDYfLHPy',
+            },
+        ]
+    },
+    {
+        categoria: 'Clubes',
+        declaraciones: [
+            {
+                titulo: 'Jockey Club Córdoba',
+                fecha: '2026-07-01',
+                link: 'https://drive.google.com/file/d/1wh82v1ksDKRRiJpTtyNnJemtqWTVIWgP',
+            },
+            {
+                titulo: 'Club Atlético Belgrano',
+                fecha: '2026-06-12',
+                link: 'https://drive.google.com/file/d/1As0xSS_QwFAmGwlTHBQiZ5fjrrKZi1Mr',
+            },
+            {
+                titulo: 'Hindú Club Córdoba',
+                fecha: '2026-07-06',
+                link: 'https://drive.google.com/file/d/17yCdEKhgqMgmigUHo6ypwc2qd-lwYdn7',
+            },
+        ]
+    },
+    {
+        categoria: 'Asociaciones Civiles, Colegios y Organizaciones No Gubernamentales',
+        declaraciones: [
+            {
+                titulo: 'Colegio de Psicólogos de la Provincia de Córdoba',
+                fecha: '2026-03-25',
+                link: 'https://drive.google.com/file/d/1PWnsohl29QhTqtwk3K6eVVsg_Z7R9KFT',
+            },
+            {
+                titulo: 'Biblioteca Popular Casa del Pueblo Alberdi',
+                fecha: '2026-03-11',
+                link: 'https://drive.google.com/file/d/1S51mLYVnllRdhZ29OnnMD3aOK4bbo895',
+            },
+            {
+                titulo: 'Biblioteca Popular Ricardo Rojas',
+                fecha: '2026-06-25',
+                link: 'https://drive.google.com/file/d/1CNXJ4_OWcPN_ASHZSpaAGwQ0_9vWRuzF',
+            },
+            {
+                titulo: 'Biblioteca Popular Rosa Areal de Molina',
+                fecha: '2026-05-06',
+                link: 'https://drive.google.com/file/d/118luVNPNDzU3WsHbC6Qn1a-WSqmAANfr',
+            },
+            {
+                titulo: 'Biblioteca Popular Restituta Albarracín de Gonzalez',
+                fecha: '2026-06-12',
+                link: 'https://drive.google.com/file/d/145CRex33kP4wO-xytGfacgwG4H38_jtj',
+            },
+            {
+                titulo: 'Biblioteca Popular del Club Social y Deportivo de Agua de Oro',
+                fecha: '2026-05-14',
+                link: 'https://drive.google.com/file/d/17gYeOUmHqH5UpCTP2mwigb-RFHX474mS',
+            },
+        ]
+    }
+]
+export default DECLARACIONES_DE_INTERES;
--- web/src/pages/KitPrensaPage.jsx
+++ web/src/pages/KitPrensaPage.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { ExternalLink, CheckCircle, Wifi, BatteryCharging, Video, Coffee, MapPin, Download, MessageSquare } from 'lucide-react';
+import { ExternalLink, CheckCircle, Wifi, BatteryCharging, Video, Coffee, MapPin, Download, MessageSquare, ChevronDown } from 'lucide-react';
 import { CountdownBanner, Navbar, FooterSection } from '@/pages/HomePage.jsx';
 import TitleSection from '@/components/TitleSection.jsx';
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
+import DECLARACIONES_DE_INTERES from '@/data/declaracionesDeInteres.js';
 
 export default function KitPrensaPage() {
+  const [expandedCategory, setExpandedCategory] = useState(null);
+
   const fadeInVariant = {
     hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.6 } }
@@ -30,7 +33,7 @@ export default function KitPrensaPage() {
           className="mb-16 bg-white/70 rounded-3xl p-6 md:p-8 border border-[#eadeed] shadow-xl shadow-[#813893]/5 backdrop-blur-sm"
         >
           <h2 className="text-2xl font-bold tracking-tight text-[#813893] mb-4 uppercase"
-           >
+          >
             Información para Medios de Comunicación
           </h2>
           <p className="text-[#343230]/80 leading-relaxed mb-4 text-base">
@@ -56,14 +59,14 @@ export default function KitPrensaPage() {
               hacé click aquí <ExternalLink size={14} style={{ display: 'inline' }} />
             </a>
           </p>
-           <h3 className="text-xl font-semibold text-[#343230] mb-4">
+          <h3 className="text-xl font-semibold text-[#343230] mb-4">
             Si venís a cubrir el entiendo, ¿Cómo te acreditás?
           </h3>
 
           <p className="text-[#343230]/80 leading-relaxed mb-6 text-base">
             Si sos periodista, comunicadora/e, fotógrafas/os y creadoras/es de contenido de medios comunitarios,
             alternativos, hegemónicos y autogestivos podés registrarte para la cobertura del evento.
-          </p> 
+          </p>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
             <div className="bg-[#faf7fb] p-4 rounded-xl border border-[#eadeed]">
@@ -72,21 +75,21 @@ export default function KitPrensaPage() {
             </div>
 
             <div className="bg-[#faf7fb] p-4 rounded-xl border border-[#eadeed] flex flex-col justify-between">
-  <span className="block text-xs font-bold uppercase tracking-wider text-gray-400">Formulario</span>
-  
-  <a
-    href="https://docs.google.com/forms/d/e/1FAIpQLSeekxXv86-me2qt6-rAI6_9uWvOleZzdDHMC8zrgYx2SwnbVw/viewform"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="inline-flex items-center gap-1.5 text-sm text-[#662c74] hover:text-[#813893] font-bold transition-colors mt-1 w-fit"
-  >
-    Registrate acá <ExternalLink size={14} />
-  </a>
-</div>
+              <span className="block text-xs font-bold uppercase tracking-wider text-gray-400">Formulario</span>
+
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSeekxXv86-me2qt6-rAI6_9uWvOleZzdDHMC8zrgYx2SwnbVw/viewform"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center gap-1.5 text-sm text-[#662c74] hover:text-[#813893] font-bold transition-colors mt-1 w-fit"
+              >
+                Registrate acá <ExternalLink size={14} />
+              </a>
+            </div>
           </div>
 
 
-           <div className="bg-[#fffcf5] border border-[#fed886] rounded-xl p-4 mb-8 text-sm text-[#6e4d05] flex gap-3 items-start">
+          <div className="bg-[#fffcf5] border border-[#fed886] rounded-xl p-4 mb-8 text-sm text-[#6e4d05] flex gap-3 items-start">
             <CheckCircle className="text-[#b57f09] shrink-0 mt-0.5" size={18} />
             <p>
               <strong className="text-[#916607]">Importante:</strong> Durante el acto de apertura se entregarán las credenciales que serán solicitadas para acceder a la sala de prensa y a las zonas para medios.
@@ -149,9 +152,9 @@ export default function KitPrensaPage() {
           initial="hidden"
           animate="visible"
           variants={fadeInVariant}
-          className="bg-white/50 rounded-3xl p-6 md:p-8 border border-[#eadeed] shadow-xl shadow-[#813893]/5"
+          className="mb-16 bg-white/50 rounded-3xl p-6 md:p-8 border border-[#eadeed] shadow-xl shadow-[#813893]/5"
         >
-          <h2 className="text-2xl font-bold tracking-tight text-[#813893] mb-1 uppercase" 
+          <h2 className="text-2xl font-bold tracking-tight text-[#813893] mb-1 uppercase"
           >
             Nuestro Kit de Prensa
           </h2>
@@ -199,7 +202,80 @@ export default function KitPrensaPage() {
               </a>
             </div>
           </div>
-        </motion.section> 
+        </motion.section>
+
+        <motion.section
+          initial="hidden"
+          animate="visible"
+          variants={fadeInVariant}
+          className="bg-white/50 rounded-3xl p-6 md:p-8 border border-[#eadeed] shadow-xl shadow-[#813893]/5"
+        >
+          <h2 className="text-2xl font-bold tracking-tight text-[#813893] mb-6 uppercase">
+            Declaraciones de Interés
+          </h2>
+          <p className="text-[#343230]/80 leading-relaxed mb-6 text-base">
+            Conocé las declaraciones de interés de las instituciones que acompañan el evento.
+          </p>
+
+          <div className="space-y-4">
+            {DECLARACIONES_DE_INTERES.map((categoria, index) => (
+              <div key={index} className="bg-[#faf7fb] rounded-xl border border-[#eadeed] overflow-hidden hover:border-[#d5bddb] transition-colors">
+                <button
+                  onClick={() => setExpandedCategory(expandedCategory === index ? null : index)}
+                  className="w-full flex items-center justify-between p-4 md:p-5 text-left hover:bg-[#f5f2f8] transition-colors"
+                >
+                  <h3 className="font-semibold text-[#343230] text-base md:text-lg">{categoria.categoria}</h3>
+                  <motion.div
+                    animate={{ rotate: expandedCategory === index ? 180 : 0 }}
+                    transition={{ duration: 0.3 }}
+                  >
+                    <ChevronDown size={20} className="text-[#813893]" />
+                  </motion.div>
+                </button>
+
+                <motion.div
+                  initial={{ height: 0, opacity: 0 }}
+                  animate={{
+                    height: expandedCategory === index ? 'auto' : 0,
+                    opacity: expandedCategory === index ? 1 : 0
+                  }}
+                  transition={{ duration: 0.3 }}
+                  className="overflow-hidden"
+                >
+                  <div className="px-4 md:px-5 pb-4 md:pb-5 border-t border-[#eadeed] bg-white/50">
+                    <div className="mt-3 space-y-3">
+                      {categoria.declaraciones.map((decl, declIndex) => (
+                        <a
+                          key={declIndex}
+                          href={decl.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="flex items-start justify-between p-3 rounded-lg bg-white border border-[#eadeed] hover:border-[#813893] hover:bg-[#faf7fb] transition-all group"
+                        >
+                          <div className="flex-1">
+                            <p className="font-medium text-[#343230] text-sm md:text-base group-hover:text-[#813893] transition-colors">
+                              {decl.titulo}
+                            </p>
+                            <p className="text-xs md:text-sm text-gray-400 mt-1">
+                              {new Date(decl.fecha).toLocaleDateString('es-AR', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric'
+                              })}
+                            </p>
+                          </div>
+                          <div className="ml-3 p-2 bg-[#eadeed] rounded-lg text-[#662c74] group-hover:bg-[#813893] group-hover:text-white shrink-0 transition-colors">
+                            <Download size={16} />
+                          </div>
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </motion.div>
+              </div>
+            ))}
+          </div>
+        </motion.section>
 
       </main>
 
